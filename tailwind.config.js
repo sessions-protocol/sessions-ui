80f5a394dcdd6f7cb4e132524d8e4c358e5d252c
JtP <jack@watchpug.org>
2022-03-22 15:13:07 +0800

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  purge: false,
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
