3594d01216f2b2c1252e94d60116ecd70f6cbbeb
JtP <jack@watchpug.org>
2022-03-28 17:28:34 +0800

    create session type

--- src/pages/SessionTypes/CreateSessionType.tsx
+++ src/pages/SessionTypes/CreateSessionType.tsx
@@ -1,59 +1,133 @@
+import sessionsABI from "@/web3/abis/sessions.json";
 import { Button } from "@chakra-ui/button";
-import { Icon } from "@chakra-ui/icon";
 import { useDisclosure } from "@chakra-ui/hooks";
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
-import { PlusIcon } from "@heroicons/react/solid";
-
+import { Icon } from "@chakra-ui/icon";
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
 import {
 	FormControl,
-	FormLabel,
-	FormErrorMessage,
-	FormHelperText,
-	Input,
-} from '@chakra-ui/react'
+	FormLabel, Input, NumberDecrementStepper,
+	NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select,
+	Textarea
+} from '@chakra-ui/react';
+import { PlusIcon } from "@heroicons/react/solid";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
+import { Field, Form, Formik } from 'formik';
+import { range } from "lodash";
+import toast from "react-hot-toast";
 
-export default function CreateSessionType() {
 
-	// const createMutation = trpc.useMutation("viewer.availability.schedule.create", {
-	// 	onSuccess: async ({ schedule }: any) => {
-	// 		await router.push("/availability/" + schedule.id);
-	// 		toast.success("Schedule created successfully");
-	// 	},
-	// 	onError: (err: any) => {
-	// 		toast.error("Schedule creation failed");
-	// 	},
-	// });
-	const create = (values: any) => {
+export default function CreateSessionType({ onCreated }: { onCreated: () => void }) {
 
-	}
-	const creating = false
 	const { isOpen, onOpen, onClose } = useDisclosure()
-
+	const durationInSlotOptions = range(1, 20)
+	const { chainId, account, library } = useWeb3React()
+	const profileId = "1243"
 	return (<>
-		<Button leftIcon={<Icon as={PlusIcon} />} colorScheme={"green"} onClick={onOpen}>New Schedule</Button>
-
-		<Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+		<Button leftIcon={<Icon as={PlusIcon} />} colorScheme={"green"} rounded={0} onClick={onOpen}>New Schedule</Button>
+		
+		<Modal blockScrollOnMount={false} closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose} >
 			<ModalOverlay />
 			<ModalContent>
-				<ModalHeader>Add a new session type</ModalHeader>
+				<ModalHeader>
+					Add a new session type
+					<p className="text-sm text-gray-500">Create a new session type for people to book times with.</p>
+				</ModalHeader>
 				<ModalCloseButton />
 				<ModalBody>
-					<div>
-						<p className="text-sm text-gray-500">Create a new session type for people to book times with.</p>
-						<FormControl>
-							<FormLabel htmlFor='title'></FormLabel>
-							<Input id='name' />
-							<FormHelperText>We'll never share your email.</FormHelperText>
-						</FormControl>
-					</div>
+					<Formik
+						initialValues={{ title: '', description: '', durationInSlot: 3, openBookingDeltaDays: 10 }}
+						validate={values => { }}
+						onSubmit={async (values, { setSubmitting }) => {
+							const signer = await library.getSigner()
+							const sessionsContract = new ethers.Contract(
+								"0xf19C27C92EEA361F8e2FD246283CD058e4d78F00",
+								sessionsABI,
+								signer
+							);
+							const calldata = [
+								profileId,
+								{
+									...values,
+									recipient: account,
+									durationInSlot: 10,
+									availabilityId: 0,
+									openBookingDeltaDays: 14,
+									token: "0x0000000000000000000000000000000000000000",
+									amount: "1000000000000000",
+									locked: false,
+									validateFollow: false,
+								},
+							];
+							const tx = await sessionsContract.createSessionType(...calldata);
+							await tx.wait();
+							toast.success("Schedule created successfully");
+							onClose();
+							setSubmitting(false);
+						}}>
+						{({ isSubmitting }) => (
+							<Form>
+								<Field name='title'>
+									{({ field, form }: any) => (
+										<FormControl className="mb-4">
+											<FormLabel htmlFor='title'>Title</FormLabel>
+											<Input {...field} id='title' placeholder="Quick chat" />
+										</FormControl>)}
+								</Field>
+								<Field name='description'>
+									{({ field, form }: any) => (
+										<FormControl className="mb-4">
+											<FormLabel htmlFor='description'>Description</FormLabel>
+											<Textarea{...field} id='description' placeholder="A quick video meeting." />
+										</FormControl>)}
+								</Field>
+								<Field name='minute'>
+									{({ field, form }: any) => (
+										<FormControl className="mb-4">
+											<FormLabel htmlFor='minute'>Length</FormLabel>
+											<Select {...field} id='minute'>
+												{durationInSlotOptions.map(d => <option key={d} value={d}>{d * 6} minutes</option>)}
+											</Select>
+										</FormControl>)}
+								</Field>
+								<Field name='openBookingDeltaDays'>
+									{({ field, form }: any) => (
+										<FormControl className="mb-4">
+											<FormLabel htmlFor='openBookingDeltaDays'>Invitees can schedule</FormLabel>
+											<NumberInput min={1}>
+												<NumberInputField {...field} id='openBookingDeltaDays' />
+												<NumberInputStepper>
+													<NumberIncrementStepper />
+													<NumberDecrementStepper />
+												</NumberInputStepper>
+											</NumberInput>
+
+										</FormControl>)}
+								</Field>
+								<div className="my-2 text-right">
+									<Button isLoading={isSubmitting} disabled={isSubmitting} type="submit" colorScheme='green' rounded={0}>
+										Continue
+									</Button>
+								</div>
+							</Form>)}
+					</Formik>
 				</ModalBody>
 
-				<ModalFooter>
-					<Button colorScheme='green' mr={3} onClick={onClose}>
-						Create
-					</Button>
-				</ModalFooter>
 			</ModalContent>
 		</Modal>
 	</>)
+}
+
+
+export interface ISessionTypeFormData {
+	title: string,
+	description: string,
+	durationInSlot: number,
+	availabilityId: number,
+	openBookingDeltaDays: number,
+	recipient: string,
+	token: string,
+	amount: number,
+	locked: boolean,
+	validateFollow: boolean
 }
