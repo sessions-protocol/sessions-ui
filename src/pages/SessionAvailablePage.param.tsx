f41a1a017485041d77928b8ab084cf72f49422b4
JtP <jack@watchpug.org>
2022-03-28 12:38:26 +0800

    update

--- src/pages/SessionAvailablePage.param.tsx
+++ src/pages/SessionAvailablePage.param.tsx
@@ -2,12 +2,14 @@ import { createPagePropsContext } from "@/context/PagePropsContext";
 import * as Yup from 'yup';
 
 export const SessionAvailablePagePropsContext = createPagePropsContext<{
+  profileId: string;
   sessionId: string;
   date?: string;
   slot?: string;
 }, {}>({
   key: 'SessionAvailablePage',
   validation: {
+    profileId: Yup.string().required(),
     sessionId: Yup.string().required(),
     date: Yup.string(),
     slot: Yup.string(),
